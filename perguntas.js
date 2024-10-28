ff710442d248e79d724598b10941c99ae55c1aca
Fernandomendesdasilva <160602665+Fernandomendesdasilva@users.noreply.github.com>
2024-10-28 15:13:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criarCARTAO(
+    'O que é Java Script?',
+    'Programação',
+    'É uma linguagem de programação'
+)
+criarCARTAO(
+    'O que é uma variável',
+    'Programação',
+    'Uma variável é um espaço que armazena dados mutáveis.'
+)
+criarCARTAO(
+    'Qual a operação contrária a subtração?',
+    'Matematica',
+    'A operação contraria é a adição.'
+)
+criarCARTAO(
+    'Quando o Brasil foi descoberto?',
+    'História',
+    'O Brasil foi descoberto em 1500.'
+)
+criarCARTAO(
+    'Qual a o nome da primeira missão que levou o homem à lua?',
+    'História',
+    'A primeira missão espacial que levou o homem ao solo lunar foi a Apollo 11.'
+)
